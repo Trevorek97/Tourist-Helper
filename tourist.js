3fa98b0e0855b16f7019c40d4921cdb8741c6a73
Trevorek97 <trevorek97@gmail.com>
2019-11-14 22:54:31 +0100

    Change menu on main page

--- tourist.js
+++ tourist.js
@@ -1,15 +1,48 @@
-var title = document.getElementById("title");
-var welcomeText = "Witaj w <b> Tourist Helper </b> - webowej aplikacji"
-    + " mającej ułatwić podróżowanie! Nie masz pomysłu dokąd wybrać"
-    + " się na urlop czy na wakacje? Planujesz weekendowy wypad poza miasto?"
-    + " Sprawdź możliwości naszej aplikacji, z nami zaplanujesz swoje najlepsze"
-    + " podróże!";
-title.addEventListener("click", function()
-{
-    location.href="mainpage.html";
-});
 function showParallelogramText()
 {
+    var welcomeText = "Witaj w <b> Tourist Helper </b> - webowej aplikacji"
+        + " mającej ułatwić podróżowanie! Nie masz pomysłu dokąd wybrać"
+        + " się na urlop czy na wakacje? Planujesz weekendowy wypad poza miasto?"
+        + " Sprawdź możliwości naszej aplikacji, z nami zaplanujesz swoje najlepsze"
+        + " podróże!";
     var text=document.getElementById("parallelogram");
     text.innerHTML=welcomeText;
 }
+
+
+function mouseOnMenu()
+{
+    var img=document.querySelectorAll(".imgmenu");
+    var pages = ["aktualnosci", "zaplanuj", "mapa", "o_nas", "kontakt"];
+    for(let i=0;i<6;i++)
+    {
+        img[i].style.filter="grayscale(1) blur(2px)";
+
+        img[i].onmouseout=function()
+        {
+            this.style.filter="grayscale(1) blur(2px)";
+        };
+        img[i].onmouseover=function()
+        {
+            this.style.filter="blur(0px)";
+
+        };
+
+        img[i].onclick=function()
+        { //test
+            window.location.href = pages[i]+ "/";
+        };
+
+    }
+}
+
+
+var tmp = document.getElementById("testowe");
+window.onload=function()
+{
+    tmp.oncontextmenu=function(e)
+    {
+        e.preventDefault();
+    };
+};
+
